cb71c34b0b25f561c43881c9eaa292de1da1496e
mrirakib04 <mdrakibulislam11928@gmail.com>
2025-03-02 23:25:51 +0600

    jwt verification added

---
+++ README.md
@@ -0,0 +1,77 @@
+# Server-3
+
+# For: web-practice-9 - HR3 Management
+
+## Links:
+
+- **Web:** [https://mrirakib-web-practice-9.netlify.app/](https://mrirakib-web-practice-9.netlify.app/)
+- **Repo:** [https://github.com/mrirakib04/web-practice-9/](https://github.com/mrirakib04/web-practice-9/)
+
+## Features
+
+- **Users:**
+
+  - Fetch all users.
+  - Fetch a specific user by email.
+  - Add a new user.
+
+- **HR Management:**
+
+  - Fetch all HRs.
+  - Fetch an HR by email.
+  - Add a new HR.
+  - Update an HR's payment status or limit.
+
+- **Employee Management:**
+
+  - Fetch all unemployed employees.
+  - Add a new unemployed employee.
+  - Remove an unemployed employee by email.
+
+- **Team Management:**
+
+  - Fetch all team members associated with an HR by email.
+  - Add an employee to a team.
+  - Remove an employee from a team by email.
+
+- **Asset Management:**
+
+  - Fetch all assets owned by a user.
+  - Fetch asset details by ID.
+  - Add a new asset.
+  - Update asset quantity.
+  - Delete an asset by ID.
+
+- **Requests Management:**
+
+  - Fetch all asset requests made by a user.
+  - Add a new asset request.
+  - Approve a pending asset request.
+  - Delete a pending asset request.
+
+- **Pending Requests:**
+
+  - Fetch all pending requests associated with a user.
+  - Fetch pending requests for a specific HR.
+  - Add a new pending request.
+  - Delete a pending request by ID.
+
+- **Payment Management:**
+  - Record a payment made to an HR.
+
+## Technologies Used
+
+- **Express.js**: Web framework for Node.js to handle HTTP requests.
+- **MongoDB**: NoSQL database for managing user, employee, asset, and request data.
+- **CORS**: Middleware to handle cross-origin requests.
+- **dotenv**: For managing environment variables like MongoDB credentials and JWT secret.
+- **JWT (JSON Web Token)**: For secure user authentication and authorization.
+- **MongoDB Client**: For interacting with the MongoDB database.
+
+## Conclusion
+
+Thank you for checking out my repository. If you have any feedback or suggestions, feel free to reach out!
+
+---
+
+Developed by **Md Rakibul Islam Rakib**
---
+++ index.js
@@ -0,0 +1,88 @@
+import express from "express";
+import cors from "cors";
+import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT || 3030;
+const app = express();
+
+// middleware
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
+app.use(express.json());
+// jwt verification
+const verifyToken = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = req.headers.authorization;
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_ACCESS}@cluster0.bfqzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
+
+    // Connections
+    const database = client.db("hr3_management");
+    const usersCollection = database.collection("users");
+    const hrsCollection = database.collection("hrs");
+    const employeesCollection = database.collection("unemployed");
+    const assignedCollection = database.collection("assigned");
+    const teamsCollection = database.collection("teams");
+    const paymentsCollection = database.collection("payments");
+    const assetsCollection = database.collection("assets");
+    const requestsCollection = database.collection("requests");
+    const pendingCollection = database.collection("pending");
+
+    // jwt
+    // jwt related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("HR3 Managements server");
+});
+
+app.listen(port, () => {
+  console.log(`HR3 Managements server listening on port ${port}`);
+});
