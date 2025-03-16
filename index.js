c74a99e60a588bb448f454b1dc2b4e1e2a6a0ce8
mrirakib04 <mdrakibulislam11928@gmail.com>
2025-03-16 21:49:55 +0600

    hr pending requests reading

--- index.js
+++ index.js
@@ -189,6 +189,13 @@ async function run() {
       const result = await pendingCollection.find(query).limit(4).toArray();
       res.send(result);
     });
+    // hr pending requests
+    app.get("/pending/hr/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { requestFor: email };
+      const result = await pendingCollection.find(query).toArray();
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
